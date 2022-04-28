23c8155afa63351a11e22b0c5c8097903b18e1eb
Hardwaregore <hardwaregore101@gmail.com>
2022-04-28 13:51:01 -0500

    a

--- init.sql
+++ init.sql
@@ -3,4 +3,17 @@
 
 CREATE DATABASE `GrassBandits`
 
-CREATE TABLE `requests` ()
+CREATE TABLE `requests` (
+    `id` int NOT NULL UNIQUE,
+    `name` varchar(255) NOT NULL,
+    `service_house_number` int(7) NOT NULL,
+    `service_street` varchar(300) NOT NULL,
+    `job_needed` varchar(50) NOT NULL,
+    `status` boolean NOT NULL,
+
+
+);
+
+ALTER TABLE `requests` 
+    ADD PRIMARY KEY (`id`);
+
