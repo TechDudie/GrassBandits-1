4d76f211e66b02696c1a9fae4b559e2a12e92857
Hardwaregore <hardwaregore101@gmail.com>
2022-04-28 15:39:34 -0500

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+
+// username here:
+$username = "";
+
+// password here!
+$password = "";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+  die("CRITICAL ERROR! CONNECTION FAILED! Err: " . $conn->connect_error);
+} 
+echo "CONNECTION SUCCESSFULL";
+?>
