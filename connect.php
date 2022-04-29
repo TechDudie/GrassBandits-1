7fe1dda4ccd88226d6ffd7adcacc6e860734a770
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-29 15:07:46 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -16,6 +16,6 @@ $conn = new mysqli($servername, $username, $password, $db_name);
 if ($conn->connect_error) {
   die("CRITICAL ERROR! CONNECTION FAILED! Err: " . $conn->connect_error);
 } 
-echo "CONNECTION SUCCESSFULL";
+echo "CONNECTION SUCCESSFUL";
 ?>
 
