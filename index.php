e9d7164a22b015bb9ad972b8d51ff0310879e077
Hardwaregore <hardwaregore101@gmail.com>
2022-04-30 14:41:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,17 +13,17 @@
 
     <form action="insert.php" method="POST">
         
-        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="name" placeholder="Name"></input>
         
-        <input type="text" name="service_house_number" placeholder="House number">
+        <input type="text" name="service_house_number" placeholder="House number"></input>
         
-        <input type="text" name="service_street_and_city" placeholder="Street, City">
+        <input type="text" name="service_street_and_city" placeholder="Street, City"></input>
         
-        <input type="text" name="job_needed" placeholder="Job Needed">
+        <input type="text" name="job_needed" placeholder="Job Needed"></input>
         
-        <input type="text" name="date_of_service" placeholder="Date">
+        <input type="text" name="date_of_service" placeholder="Date"></input>
         
-        <input type="text" name="time_of_service" placeholder="Time">
+        <input type="text" name="time_of_service" placeholder="Time"></input>
         
         <button type="submit" name="submit">Submit</button>
         
