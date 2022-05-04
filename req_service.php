66e09fd0815e99471bddddc9410ad4e8d66288d2
Hardwaregore <hardwaregore101@gmail.com>
2022-05-04 16:52:45 -0500

    Update req_service.php

--- req_service.php
+++ req_service.php
@@ -17,15 +17,13 @@
         
         <input type="text" name="name" placeholder="Name"></input>
         
-        <input type="text" name="service_house_number" placeholder="House number"></input>
+        <input type="text" name="contact_info" placeholder="Contact info"></input>
         
-        <input type="text" name="service_street_and_city" placeholder="Street, City"></input>
+        <input type="text" name="service_address" placeholder="Street, City"></input>
         
         <input type="text" name="job_needed" placeholder="Job Needed"></input>
         
-        <input type="text" name="date_of_service" placeholder="Date"></input>
-        
-        <input type="text" name="time_of_service" placeholder="Time"></input>
+        <input type="datetime-local" name="date" placeholder="Date and time"></input>
         
         <button type="submit" name="submit">Submit</button>
         
