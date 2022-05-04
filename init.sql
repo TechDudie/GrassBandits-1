6cf1cf5d2df522000e69f0133cabf240fa0e17ce
Hardwaregore <hardwaregore101@gmail.com>
2022-05-04 16:38:18 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -3,29 +3,41 @@
 
 CREATE DATABASE `GrassBandits`;
 
+USE `GrassBandits`
+
 CREATE TABLE `requests` (
     `id` int NOT NULL UNIQUE AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `service_house_number` int(7) NOT NULL,
-    `service_street_and_city` varchar(300) NOT NULL,
+    `service_address` varchar(200) NOT NULL,
     `job_needed` varchar(50) NOT NULL,
-    `date` date NOT NULL,
-    `time` int NOT NULL UNIQUE,
-    `status` boolean NOT NULL
+    `date` varchar(100) NOT NULL,
+
 
 
 );
 
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+    
+);
+
 CREATE TABLE `staff_login` (
-    `id` int NOT NULL UNIQUE AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `uname` int(7) NOT NULL,
-    `password` varchar(300) NOT NULL
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL
+
+);
 
 
 );
 
 ALTER TABLE `requests` 
+    ADD PRIMARY KEY (`id`);
+    
+ALTER TABLE `users` 
     ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `staff_login` 
