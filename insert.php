4da30e9803bd7c86222a5e8983002a32f916519c
Hardwaregore <hardwaregore101@gmail.com>
2022-05-04 16:52:25 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,14 +3,13 @@
     include_once 'connect.php';
 
     $name = $_POST['name'];
-    $house_number = $_POST['service_house_number'];
-    $street_city = $_POST['service_street_and_city'];
+    $contact = $_POST['contact_info'];
+    $address = $_POST['service_house_number'];
     $job = $_POST['job_needed'];
-    $date = $_POST['date_of_service'];
-    $time = $_POST['time_of_service'];
+    $date = $_POST['time_of_service'];
 
-    $write = "INSERT INTO `requests` (`name`, `service_house_number`, `service_street_and_city`, `job_needed`, `date`, `time`, `status`) 
-    VALUES ('$name', '$house_number', '$street_city', '$job', '$date', '$time', false);";
+    $write = "INSERT INTO `requests` (`name`, `service_address`, `contact_info`, `job_needed`, `date`, `status`) 
+    VALUES ('$name', '$address', '$contact', '$job', '$date', false);";
 
     mysqli_query($conn, $write);
 
