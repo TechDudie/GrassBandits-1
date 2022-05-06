79ebe17dbf0a9418940f4f809e07accd80d731fd
Hardwaregore <hardwaregore101@gmail.com>
2022-05-05 22:27:15 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -22,8 +22,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <h1 class="my-5">Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Say hello to GrassBandits!</h1>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+     
     </p>
 </body>
 </html>
