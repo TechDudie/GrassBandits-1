a47ba056c668e9a84569532308f8c1670d61357e
Hardwaregore <hardwaregore101@gmail.com>
2022-05-06 17:44:25 -0500

    Update login.php

--- login.php
+++ login.php
@@ -95,7 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; text-align: center;}
+        body{ font: 14px sans-serif; justify-content: center;}
         .wrapper{ width: 360px; padding: 20px; }
     </style>
 </head>
