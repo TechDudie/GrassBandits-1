f0c0f8ca221c9bbaa56f1591fa4457f6a849d51a
Hardwaregore <hardwaregore101@gmail.com>
2022-05-06 17:34:02 -0500

    Update insert-account.php

--- insert-account.php
+++ insert-account.php
@@ -1,1 +1,23 @@
+<?php // Personal Home Page (PHP)
+    
+    include_once 'connect.php';
 
+
+    $contact = $_POST['contact_info'];
+    $address = $_POST['service_address'];
+    $job = $_POST['job_needed'];
+    $date = $_POST['date'];
+
+    $write = "INSERT INTO `requests` (`name`, `service_address`, `contact_info`, `job_needed`, `date`, `status`) 
+    VALUES ('$_SESSION["username"]', '$address', '$contact', '$job', '$date', false);";
+
+    mysqli_query($conn, $write);
+
+    header("Location: thanks.html");
+
+
+
+
+
+        
+?>
