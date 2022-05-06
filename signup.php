c95c94be7087e5d1579f7d3e8f6beb7feecafd48
Hardwaregore <hardwaregore101@gmail.com>
2022-05-06 17:42:40 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -107,7 +107,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </style>
 </head>
 <body>
-    <div class="wrapper">
+    <div class="wrapper" style="text-align: center;">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
